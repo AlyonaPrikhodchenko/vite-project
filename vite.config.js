bf768f9a9edc8840b938e4d0443e3a5b79c2f673
Alyona <alenka.inform@gmail.com>
2023-04-20 19:01:29 +0300

    Добавляет комментарии

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,26 @@
+//Импорты пакетов
+
 // import {resolve} from 'path';
 // import {defineConfig} from 'vite';
 // import vitePugPlugin from 'vite-plugin-pug-transformer';
 import pugPlugin from 'vite-plugin-pug';
 
+// Путь к корню проекта и к папке сборки
+
 // const root = resolve(__dirname, 'src');
 // const outDir = resolve(__dirname, 'dist');
 
-import vitePugPlugin from 'vite-plugin-pug-transformer';
-
+// Хз что это
 const locals = {bundler: 'Vite'};
-const root = resolve(__dirname, 'src');
 
 export default {
   root,
   // outDir: './dist',
   // base: './src',
+
   publicDir: './src/assets',
 
+  //Настройки при сборке
   build: {
     rollupOptions: {
       input: {
@@ -25,195 +29,205 @@ export default {
       },
     },
 
+    // Плагины
     plugins: [vitePugPlugin({pugLocals: locals}), pugPlugin()],
   }
 }
 
-// export default defineConfig(({ command, mode, ssrBuild }) => {
-//   if (command === 'serve') {
-//     return {
-//       // dev specific config
-//     }
-//   } else {
-//     // command === 'build'
-//     return {
-//       // build specific config
-//     }
-//   }
-// })
+/* Это не пробовала, тут настройки в зависимости от условия можно прописывать (сервер запускаешь или билд)
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  if (command === 'serve') {
+    return {
+      // dev specific config
+    }
+  } else {
+    // command === 'build'
+    return {
+      // build specific config
+    }
+  }
+})
+*/
 
-// export default defineConfig({
-//   build: {
-//     outDir
-//   //   // rollupOptions: {
-//   //   //   input: {
-//   //   //     main: './src/index.html',
-//   //   //   }
-//   //   // }
-//   // },
+/* export default defineConfig({
+  build: {
+    outDir
+    rollupOptions: {
+      input: {
+        main: './src/index.html',
+      }
+    }
+  },
 
-//   server: {
-//     open: true
-//   },
+С этой настройкой сервер автоматически запускается
+  server: {
+    open: true
+  },
 
-//   plugins: [pugPlugin()]
+  plugins: [pugPlugin()]
 
-// });
+});
 
-// export default defineConfig({
-//   base: '/',
-//   build: {
-//     root,
-//     outDir,
-//     assetsDir: 'assets',
+export default defineConfig({
+  base: '/',
+  build: {
+    root,
+    outDir,
+    assetsDir: 'assets',
 
-//     rollupOptions: {
-//       input: {
-//         main: resolve(root, 'index.html'),
-//       },
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+      },
+*/
 
-//       output: {
-//         // chunkFileNames: "js/[name]-[hash].js",
-//         // entryFileNames: "js/[name]-[hash].js",
-//         assetFileNames: ({name}) => {
-//           if (/\.(gif|jpe?g|png|svg)$/.test(name ?? "")) {
-//             return "assets/images/[name]-[hash][extname]";
-//           }
+/* Как я поняла, здесь прописывать можно что будет на выходе, сам код из чьей-то сборки на гитхабе
+      output: {
+        chunkFileNames: "js/[name]-[hash].js",
+        entryFileNames: "js/[name]-[hash].js",
+        assetFileNames: ({name}) => {
+          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? "")) {
+            return "assets/images/[name]-[hash][extname]";
+          }
 
-//           if (/\.css$/.test(name ?? "")) {
-//             return "css/[name]-[hash][extname]";
-//           }
+          if (/\.css$/.test(name ?? "")) {
+            return "css/[name]-[hash][extname]";
+          }
 
-//           if (/\.woff|2$/.test(name ?? "")) {
-//             return "assets/fonts/[name]-[extname]";
-//           }
+          if (/\.woff|2$/.test(name ?? "")) {
+            return "assets/fonts/[name]-[extname]";
+          }
 
-//           return "assets/[name]-[hash][extname]";
-//         },
-//       },
-//     }
-//   },
+          return "assets/[name]-[hash][extname]";
+        },
+      },
+    }
+  },
 
-//   server: {
-//     open: true
-//   },
+  server: {
+    open: true
+  },
 
-//   css: {
-//     devSourcemap: true,
-//   },
+  css: {
+    devSourcemap: true,
+  },
 
-//   plugins: [vitePugPlugin()]
+  plugins: [vitePugPlugin()]
 
-// });
+});
+*/
 
-// const htmlPlugin = () => {
-//   return {
-//     name: 'html-transform',
-//     transformIndexHtml(html) {
-//       return html.replace(/\.pug/, `.html`)
-//     }
-//   }
-// }
+/*transformIndexHtml - хук для компиляции паг в html, но я хз как применить
 
-// export default {
-//   plugins: [vitePugPlugin(htmlPlugin)],
-// };
-
-// `./${page.replace(/\.pug/, `.html`)}`
-
-// const root = resolve(__dirname, 'src/pages');
-// const outDir = resolve(__dirname, 'dist');
-
-// export default defineConfig({
-//   root,
-//   outDir,
-
-//   rollupOptions: {
-//     input: {
-//       main: resolve(root, 'index.html'),
-//       about: resolve(root, 'main.html')
-//     },
-//   }
-// });
+const htmlPlugin = () => {
+  return {
+    name: 'html-transform',
+    transformIndexHtml(html) {
+      return html.replace(/\.pug/, `.html`)
+    }
+  }
+}
+*/
 
 
-// export default defineConfig({
-//   plugins: [pug()],
-//   build: {
-//     rollupOptions: {
-//       input: 'src/pages/index.pug',
-//       output: {
-//         file: 'dist/index.html',
-//       },
-//     },
-//   },
-// });
+/*
+export default {
+  plugins: [vitePugPlugin(htmlPlugin)],
+};
 
-// module.exports = {
-//   chainWebpack: (config) => {
-//     config.module.rule('pug')
-//       .test(/\.pug$/)
-//       .use('pug-plain-loader')
-//       .loader('pug-plain-loader')
-//       .end();
-//   }
-// }
+`./${page.replace(/\.pug/, `.html`)}`
 
-// let source = pug.renderFile('src/pages/main.pug', {doctype: 'html'});
+const root = resolve(__dirname, 'src/pages');
+const outDir = resolve(__dirname, 'dist');
 
-// const options = {
-//   doctype: source,
-// }
-// const locals = {name: "My Pug"}
+export default defineConfig({
+  root,
+  outDir,
 
-// export default defineConfig({
-//   plugins: [pugPlugin(options, locals)],
-// })
-
-// const locals = {bundler: 'Vite'};
-// const root = resolve(__dirname, 'src');
-// const outDir = resolve(__dirname, 'dist');
+  rollupOptions: {
+    input: {
+      main: resolve(root, 'index.html'),
+      about: resolve(root, 'main.html')
+    },
+  }
+});
+*/
 
 
-// export default defineConfig({
-//   root,
+/* export default defineConfig({
+  plugins: [pug()],
+  build: {
+    rollupOptions: {
+      input: 'src/pages/index.pug',
+      output: {
+        file: 'dist/index.html',
+      },
+    },
+  },
+});
+*/
 
-//   plugins: [vitePugPlugin({pugLocals: locals})],
 
-//   build: {
-//     outDir,
-//     emptyOutDir: true,
-//     rollupOptions: {
-//       input: {
-//         main: resolve(root, 'index.html'),
-//         about: resolve(root, 'pages', 'main.html')
-//       }
-//     }
-//   },
-// });
+/* let source = pug.renderFile('src/pages/main.pug', {doctype: 'html'});
 
-// import {defineConfig} from 'vite'
-// import pug from 'vite-plugin-pug'
+const options = {
+  doctype: source,
+}
+const locals = {name: "My Pug"}
 
-// export default defineConfig({
-//   plugins: [
-//     pug(
-  // {
-//   basedir: 'pages',
-//   pretty: true,
-//   outDir: 'public'
-// }
-)
-//   ],
+export default defineConfig({
+  plugins: [pugPlugin(options, locals)],
+})
+*/
 
-//   build: {
-//     outDir,
-//     // emptyOutDir: true,
-//     rollupOptions: {
-//       input: {
-//         main: resolve(root, 'index.html'),
-//         about: resolve(root, 'pages', 'main.html')
-//       }
-//     }
-//   },
-// })
+
+/* 
+
+const locals = {bundler: 'Vite'};
+const root = resolve(__dirname, 'src');
+const outDir = resolve(__dirname, 'dist');
+
+export default defineConfig({
+  root,
+
+  plugins: [vitePugPlugin({pugLocals: locals})],
+
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        about: resolve(root, 'pages', 'main.html')
+      }
+    }
+  },
+});
+
+*/
+
+
+
+/*
+export default defineConfig({
+  plugins: [
+    pug(
+  {
+  basedir: 'pages',
+  pretty: true,
+  outDir: 'public'
+})
+  ],
+
+  build: {
+    outDir,
+    // emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        about: resolve(root, 'pages', 'main.html')
+      }
+    }
+  },
+})
+*/
